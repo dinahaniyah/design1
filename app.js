87e421b70ce386362a3cc6116acb581feb034b62
Dina Haniyah <haniyah.dina@gmail.com>
2021-06-14 14:53:12 +0700

    u

--- app.js
+++ app.js
@@ -39,23 +39,6 @@ $(document).ready(function () {
       // Do something for an error here
     });
 
-  $.getJSON("data/mainData.json", function (data) {
-    var groomNick = "";
-    var brideNick = "";
-    var image_1 = "";
-
-    $.each(data, function (key, value) {
-      groomNick += value.nick_groom;
-      brideNick += value.nick_bride;
-      // image_1 += value.image_start;
-    });
-    $(".groom_name").append(groomNick);
-    $(".bride_name").append(brideNick);
-    // $(".content-onload").css("background", "pink !important");
-
-    // $(groomName).name_groom.appendTo;
-  });
-
   // PARTICLE JS
   $(".header").each(function () {
     particlesJS("particles-js", {
