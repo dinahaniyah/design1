d765b79912aed69cc729ac52fb1f6837aba72589
Dina Haniyah <haniyah.dina@gmail.com>
2021-06-17 00:49:04 +0700

    new design 2

--- app.js
+++ app.js
@@ -232,6 +232,33 @@ $(document).ready(function () {
       })
       .catch((error) => console.error("Error!", error.message));
   });
+
+  // ========================= GALERY =====================
+  $("#animated-thumbnails-gallery")
+    .justifiedGallery({
+      captions: false,
+      lastRow: "hide",
+      rowHeight: 180,
+      margins: 5,
+    })
+    .on("jg.complete", function () {
+      window.lightGallery(
+        document.getElementById("animated-thumbnails-gallery"),
+        {
+          autoplayFirstVideo: false,
+          pager: false,
+          galleryId: "nature",
+          plugins: [lgZoom, lgThumbnail],
+          download: false,
+          mobileSettings: {
+            controls: false,
+            showCloseIcon: false,
+            download: false,
+            rotate: false,
+          },
+        }
+      );
+    });
 });
 
 // COUNTDOWN
